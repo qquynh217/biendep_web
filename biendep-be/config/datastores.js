777a29e60f9a095efd89bc2092ad3829ccb84412
qquynh217 <nltquynh875@gmail.com>
2025-04-19 01:09:58 +0700

    update: datastore

--- biendep-be/config/datastores.js
+++ biendep-be/config/datastores.js
@@ -47,6 +47,7 @@ module.exports.datastores = {
      *                                                                          *
      ***************************************************************************/
     adapter: require("sails-mongo"),
-    url: `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
+    // url: `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
+    url: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`,
   },
 };
