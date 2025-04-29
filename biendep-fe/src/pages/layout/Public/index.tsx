bd7ddb971ecf07f90a9435bc67fe70ba375437f6
nltquynh875 <nltquynh875@gmail.com>
2025-04-29 11:00:43 +0700

    update(layou): add phone float button

--- biendep-fe/src/pages/layout/Public/index.tsx
+++ biendep-fe/src/pages/layout/Public/index.tsx
@@ -1,6 +1,6 @@
 import { FloatButton } from "antd";
 import type { FC } from "react";
-import { FaFacebookF } from "react-icons/fa6";
+import { FaFacebookF, FaPhone } from "react-icons/fa6";
 import { SiZalo } from "react-icons/si";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
@@ -12,20 +12,29 @@ const PublicLayout: FC = () => {
       <Header />
       <Outlet />
       <Footer />
-      <FloatButton.Group shape="circle" style={{ insetInlineEnd: 16, bottom: 70 }}>
+      <FloatButton.Group
+        shape="circle"
+        style={{ insetInlineEnd: 16, bottom: 70 }}
+      >
         <FloatButton
           icon={<FaFacebookF />}
           href={import.meta.env.VITE_FB}
           target="_blank"
-          className="fb_link"
+          className="app-float-btn fb_link"
         />
         <FloatButton
           icon={<SiZalo />}
           href={import.meta.env.VITE_ZALO}
           target="_blank"
-          className="zalo_link"
+          className="app-float-btn zalo_link"
         />
-        <FloatButton.BackTop visibilityHeight={0} />
+        <FloatButton
+          icon={<FaPhone />}
+          href={import.meta.env.VITE_PHONE}
+          target="_blank"
+          className="app-float-btn phone_link"
+        />
+        {/* <FloatButton.BackTop visibilityHeight={0} /> */}
       </FloatButton.Group>
     </>
   );
